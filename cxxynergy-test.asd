476324beb93e11006c8004eac7504b38c7805082
Junker <1144095+Junker@users.noreply.github.com>
2026-04-08 19:20:56 +0300

    fix tests

--- cxxynergy-test.asd
+++ cxxynergy-test.asd
@@ -3,19 +3,19 @@
   Copyright (c) 2021 Islam Omar (io1131@fayoum.edu.eg)
 |#
 
-(defpackage :cxx-jit/test/system
+(defpackage :cxxynergy/test/system
   (:use :cl :asdf :uiop))
 
-(in-package :cxx-jit/test/system)
+(in-package :cxxynergy/test/system)
 
-(defsystem :cxx-jit-test
+(defsystem :cxxynergy-test
   :author "Islam Omar"
   :license "MIT"
-  :depends-on (:cxx-jit
+  :depends-on (:cxxynergy
                :rove)
   :components ((:module "tests"
                 :components
                 ((:file "functions-test"))))
-  :description "Test system for cxx-jit"
+  :description "Test system for cxxynergy"
 
   :perform (test-op (op c) (symbol-call :rove '#:run c)))
--- tests/functions-test.lisp
+++ tests/functions-test.lisp
@@ -1,16 +1,38 @@
-(defpackage cxx-jit/test
-  (:use :cl
-        :rove
-        :cxx-jit
-        ))
-(in-package :cxx-jit/test)
+(defpackage cxxynergy/test
+  (:use :cl :rove :cxxynergy))
+(in-package :cxxynergy/test)
 
-(deftest example-test
-  (from '("<string>") 'import '("[](std::string x){return \"Hi, \"+x;}" . "hi"))
-  (from '("<cmath>") 'import '("static_cast<double(*)(double)>(std::sin)" . "cpp-sin"))
-  (testing "correctness"
-  (ok (string= (hi "there!") "Hi, there!"))
-  (ok (= (cpp-sin 0d0) 0d0))
-  (ok (= (cpp-sin (/ pi 2)) 1d0))))
+(deftest string-example
+  (testing "string manipulation"
+           (with-cxx ("<string>")
+             (defcxxfun hi "[](std::string x){return \"Hi, \"+x;}" ))
+           (ok (string= (hi "there!") "Hi, there!"))))
+
+(deftest math-example
+  (testing "math functions"
+           (with-cxx ("<cmath>")
+             (defcxxfun cpp-sin "static_cast<double(*)(double)>(std::sin)"))
+           (ok (= (cpp-sin 0d0) 0d0))
+           (ok (= (cpp-sin (/ pi 2)) 1d0))))
+
+(deftest multiple-functions
+  (testing "importing multiple functions"
+           (with-cxx ("<cmath>")
+             (defcxxfun cpp-sqrt "static_cast<double(*)(double)>(std::sqrt)")
+             (defcxxfun cpp-exp "static_cast<double(*)(double)>(std::exp)")
+             (defcxxfun cpp-log "static_cast<double(*)(double)>(std::log)"))
+           (ok (= (cpp-sqrt 4d0) 2d0))
+           (ok (> (cpp-exp 1d0) 2.717d0))
+           (ok (< (cpp-exp 1d0) 2.719d0))))
+
+(deftest raw-code-injection
+  (testing "raw C++ code injection"
+    (with-cxx ()
+      (cxx-raw "struct C { auto hi(){return \"Hello World!\";} auto bye(){return \"Bye\";}; };")
+      (defcxxfun hi "&C::hi")
+      (defcxxfun bye "&C::bye")
+      (defcxxfun get-c "[](){static C x; return x;}"))
+    (let ((obj (get-c)))
+      (ok (string= (hi obj) "Hello World!")))))
 
 (run-suite *package*)
